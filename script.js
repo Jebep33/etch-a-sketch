db34b8d0ceaf6594d5e10947f8d0a47d8c4d6fc6
Jerome <87921319+Jebep33@users.noreply.github.com>
2025-03-07 22:40:38 +0800

    Completed all objects, except optional objectives

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const button = document.querySelector("button");
+const container = document.querySelector(".container");
+let num = 0;
+
+button.addEventListener("click", () => {
+
+    while(container.hasChildNodes()){
+        container.removeChild(document.querySelector(".row"));
+    }
+    
+    do{
+        num = parseInt(prompt("What is the size of your canvass?"), 10);
+        if(isNaN(num) || num < 1 || num > 100){
+            alert("Please enter a number between 1-100");
+        }
+    } while(isNaN(num) || num < 1 || num > 100)
+
+    for(let x = 0; x < num; x++){
+        let rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class","row");
+        container.appendChild(rowDiv);
+        for(let y = 0; y < num; y++){
+            let grid = document.createElement("div");
+            grid.setAttribute("class","grid");
+            rowDiv.appendChild(grid);
+        }
+    }
+
+    let squares = document.querySelectorAll(".grid");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => { 
+            let red = Math.floor(Math.random()*255);
+            let green = Math.floor(Math.random()*255);
+            let blue = Math.floor(Math.random()*255);
+            square.setAttribute("style",`background-color: rgba(${red},${green},${blue});`);  
+        });
+    });
+
+});
+
+
+
+
+
+
+
